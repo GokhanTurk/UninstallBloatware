f816a407ef94057a5dde5bfab8657ea620263331
Gökhan Türk <ensar.gokhan@hotmail.com>
2023-01-27 09:33:33 +0300

    Create uninstall.ps1

---
+++ uninstall.ps1
@@ -0,0 +1,27 @@
+if (-NOT ([Security.Principal.WindowsPrincipal] [Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
+Write-Warning "You must run it as administrator!"
+Ping 127.0.0.1 | Out-Null
+Exit
+}
+if (!(Get-Command winget -ErrorAction SilentlyContinue)) {
+irm "https://raw.githubusercontent.com/GokhanTurk/FormatSonrasi.bat/main/SilentWinget.ps1" | iex
+}
+winget install rarlab.winrar -h --accept-source-agreements --accept-package-agreements
+winget uninstall Microsoft.SkypeApp_kzf8qxf38zg5c -h
+winget uninstall Microsoft.XboxApp_8wekyb3d8bbwe -h
+winget uninstall Microsoft.MicrosoftSolitaireCollection_8wekyb3d8bbwe -h
+winget uninstall Microsoft.MixedReality.Portal_8wekyb3d8bbwe -h
+winget uninstall Microsoft.SkypeApp_kzf8qxf38zg5c -h
+winget uninstall SpotifyAB.SpotifyMusic_zpdnekdrzrea0 -h
+winget uninstall Microsoft.XboxGamingOverlay_8wekyb3d8bbwe -h
+winget uninstall Microsoft.YourPhone_8wekyb3d8bbwe -h
+winget uninstall Microsoft.549981C3F5F10_8wekyb3d8bbwe -h
+winget uninstall Microsoft.XboxGameOverlay_8wekyb3d8bbwe -h
+winget uninstall Microsoft.Xbox.TCUI_8wekyb3d8bbwe -h
+winget uninstall Microsoft.BingFinance_8wekyb3d8bbwe -h
+winget uninstall Microsoft.BingSports_8wekyb3d8bbwe -h
+winget uninstall Microsoft.People_8wekyb3d8bbwe -h
+winget uninstall Disney.37853FC22B2CE_6rarf9sa4v8jt -h
+
+Pause
+Exit
